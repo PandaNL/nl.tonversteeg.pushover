d6565417fd21eb1d02cba052c6dce8866779ce5e
Ton Versteeg <tonversteeg1@gmail.com>
2018-04-15 16:05:44 +0200

    Version 0.0.9

--- README.md
+++ README.md
@@ -1,28 +1,5 @@
-# Pushover notifications for Athom Homey
 
-This app lets you send Pushover notifications to use in flows on a Homey device (by Athom).
+# Pushover-Client
 
-1. Go to Pushover.net and login
-2. Grab your user key
-3. Create a new application for Homey https://pushover.net/apps/build
-4. Grab your created application key
-5. Go to settings on your Homey, and under Pushover Notifications fill in your user & application/token key and save.
+A homey pushover notification client with image support
 
-### Donate
-If the Pushover app is useful to you, buy me a beer!
-
-[![Paypal donate][pp-donate-image]][pp-donate-link]
-
-[pp-donate-link]: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=D8RA9P824YZ62&lc=NL&item_name=Pushover%2dHomey&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
-[pp-donate-image]: https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif
-
-### Changelog
-
-0.0.8
-
-- Added option to select receiving sound (contribution by Dannzak)
-
-0.0.7
-
-- Added check to see if Message isn't empty to fix app crash
-- Added Insight logging support
--- app.js
+++ app.js
@@ -1,266 +1,292 @@
-"use strict";
+'use strict';
 
-var push = require('pushover-notifications');
-var http = require('http.min');
-var account = [];
-var request = [];
-var validation;
-var devices = null;
-var pushoverUser = null;
-var pushoverToken = null;
-var ledringPreference = false;
+const Homey = require('homey');
+let push = require('pushover-notification-client');
+let http = require('http.min');
+let account = [];
+let request = [];
+let validation;
+let devices = null;
+let pushoverUser = null;
+let pushoverToken = null;
+let ledringPreference = false;
+let InsightLog = null;
 
-// Get accounts from homey settings page.
+class MyApp extends Homey.App {
+
+	onInit() {
+
+        // Start building Pushover accounts array
+		buildPushoverArray();
+
+		createInsightlog();
+
+		Homey.ManagerSettings.on('set', function (settingname) {
+
+			if (settingname == 'pushoveraccount') {
+				console.log('Pushover - Account has been changed/updated...');
+				buildPushoverArray();
+			}
+		});
+
+		let sendMessageDevice = new Homey.FlowCardAction('pushoverSend_device');
+		sendMessageDevice
+		    .register()
+		    .registerRunListener(( args, state ) => {
+
+                if (typeof validation == 'undefined' || validation == '0') return new Error("Pushover api/token key not configured or valid under settings!");
+                let tempUser = pushoverUser;
+                let tempToken = pushoverToken;
+                let pMessage = args.message;
+                let sound = args.sound
+                if (typeof pMessage == 'undefined' || pMessage == null || pMessage == '') return new Error("Message can not be empty");
+                let pDevice = args.device.name;
+                if (pDevice == null || pDevice == '') return new Error("No devices registered on this Pushover account!");
+                let pPriority = args.priority;
+				return pushoverSend_device(tempUser, tempToken, pMessage, pDevice, pPriority, sound);
+				//return Promise.resolve();
+		    })
+		    //.getArgument('to')
+	        //.registerAutocompleteListener(( query, args ) => {
+            //    let deviceSearchString = value.query;
+            //    let items = searchForDevicesByValue(deviceSearchString);
+            //    return Promise.resolve(null, items);
+            //});
+            let sendMessage = new Homey.FlowCardAction('pushoverSend');
+            sendMessage
+                .register()
+                .registerRunListener(( args, state ) => {
+
+                    if (typeof validation == 'undefined' || validation == '0') return new Error("Pushover api/token key not configured or valid under settings!");
+                    let tempUser = pushoverUser;
+                    let tempToken = pushoverToken;
+                    let pMessage = args.message;
+                    let sound = args.sound;
+                    if (typeof pMessage == 'undefined' || pMessage == null || pMessage == '') return new Error("Message can not be empty");
+                    let pPriority = args.priority;
+                    return pushoverSend(tempUser, tempToken, pMessage, pPriority, sound);
+                    //return Promise.resolve();
+                })
+                //.getArgument('to')
+                //.registerAutocompleteListener(( query, args ) => {
+                //   let deviceSearchString = value.query;
+                //    let items = searchForDevicesByValue(deviceSearchString);
+                //    return Promise.resolve(null, items);
+                //});
+
+		let sendImage = new Homey.FlowCardAction('pushoverSendImage');
+		sendImage
+			.register()
+			.registerRunListener(( args, state) => {
+
+                if (typeof validation == 'undefined' || validation == '0') return callback(new Error("Pushover api/token key not configured or valid under settings!"));
+                let tempUser = pushoverUser;
+                let tempToken = pushoverToken;
+								let pMessage = args.message;
+								let pPriority = args.priority;
+                let image = args.droptoken;
+				image.getBuffer()
+				.then( buf => {
+                    //console.log(buf);
+					return pushoverSend(tempUser, tempToken, pMessage, pPriority, null, buf);
+                })
+				.catch (function(err){
+					console.log(err)
+				})
+
+			})
+			//.getArgument('to')
+	        //.registerAutocompleteListener(( query, args ) => {
+	        //    return Promise.resolve(chat_ids);
+	        //});
+	}
+
+}
+
+
+// Send notification with parameters
+function pushoverSend(pUser, pToken, pMessage, pPriority, sound, image) {
+	let priority = 0;
+	switch (pPriority) {
+		case 'Normal':
+			priority = 0;
+			break;
+		case 'Lowest':
+			priority = -2;
+			break;
+		case 'Low':
+			priority = -1;
+			break;
+		case 'High':
+			priority = 1;
+			break;
+	}
+	if (pToken != "") {
+
+		let p = new push({
+			user: pUser,
+			token: pToken,
+		});
+
+		let msg = {
+			// These values correspond to the parameters detailed on https://pushover.net/api
+			// 'message' is required. All other values are optional.
+			message: pMessage,   // required
+			title: "Homey",
+			priority: priority,
+			sound: sound,
+			attachment: image
+		};
+
+		p.send(msg, function (err, result) {
+			if (err) {
+				throw err;
+			} else {
+				if (ledringPreference == true) {
+					LedAnimate("green", 3000);
+				}
+			}
+			console.log(result);
+			//Add send notification to Insights
+			InsightEntry(1, new Date());
+		});
+	} else {
+		if (ledringPreference == true) {
+			LedAnimate("red", 3000);
+		}
+	}
+}
+
+function InsightEntry(message, date)
+{
+
+	Homey.ManagerInsights.getLog('pushover_sendNotifications').then(logs => {
+		logs.createEntry(message,date).catch( err => {
+			console.error(err);
+		});
+	}).catch(err => {
+	console.log("Cannot Make insight entry")
+	});
+}
+
+// Send notification with parameters
+function pushoverSend_device(pUser, pToken, pMessage, pDevice, pPriority, sound, image) {
+	let priority = 0;
+	switch (pPriority) {
+		case 'Normal':
+			priority = 0;
+			break;
+		case 'Lowest':
+			priority = -2;
+			break;
+		case 'Low':
+			priority = -1;
+			break;
+		case 'High':
+			priority = 1;
+			break;
+	}
+	if (pToken != "") {
+		let p = new push({
+			user: pUser,
+			token: pToken,
+		});
+
+		let msg = {
+			// These values correspond to the parameters detailed on https://pushover.net/api
+			// 'message' is required. All other values are optional.
+			message: pMessage,   // required
+			title: "Homey",
+			device: pDevice,
+			priority: priority,
+			sound: sound,
+			attachment: image
+		};
+
+		p.send(msg, function (err, result) {
+			if (err) {
+				return Promise.reject(err);
+				//throw err;
+			} else {
+				return Promise.resolve(result);
+				if (ledringPreference == true) {
+					LedAnimate("green", 3000);
+				}
+			}
+			console.log(result);
+		});
+	} else {
+		if (ledringPreference == true) {
+			LedAnimate("red", 3000);
+		}
+	}
+}
+
+
+// Create Insight log
+function createInsightlog() {
+
+	Homey.ManagerInsights.createLog('pushover_sendNotifications', {
+		label: {
+			en: 'Send Notifications'
+		},
+		type: 'number',
+		units: {
+			en: 'notifications'
+		},
+		decimals: 0
+	}).then(function (err){
+	console.log("Log Created")
+	}).catch(function (err)
+{
+	console.log("Log Not created. " + err)
+});
+}
+
 function buildPushoverArray() {
 	account = null;
-	account = Homey.manager('settings').get('pushoveraccount');
+	account = Homey.ManagerSettings.get('pushoveraccount');
 
 	if (account != null) {
 		pushoverUser = account['user'];
 		pushoverToken = account['token'];
 		ledringPreference = account['ledring'];
 
-		var url = "https://api.pushover.net/1/users/validate.json";
+		let url = "https://api.pushover.net/1/users/validate.json";
 
 		http.post(url, 'token=' + pushoverToken + '&user=' + pushoverUser).then(function (result) {
-			if(result.response.statusCode == "200"){
+			if (result.response.statusCode == "200") {
 				request = JSON.parse(result.data);
 				devices = request.devices;
 				validation = request.status;
 
-				if (validation == "1"){
-					Homey.log("Pushover - Account validated successful");
-					Homey.log("Pushover - Listing devices: " + devices);
+				if (validation == "1") {
+					console.log("Pushover - Account validated successful");
+					console.log("Pushover - Listing devices: " + devices);
 					logValidation();
 				} else {
-					Homey.log("Pushover - User and/or Token key invalid");
+					console.log("Pushover - User and/or Token key invalid");
 				}
 			} else if (result.response.statusCode == "400") {
-				Homey.log("Pushover - User and/or Token key invalid");
+				console.log("Pushover - User and/or Token key invalid");
 				validation = "0";
 				logValidation();
 			}
 		});
 
 	} else {
-	Homey.log("Pushover - No account configured yet");
+		console.log("Pushover - No account configured yet");
 	}
 }
 
 function logValidation() {
-	var validatedSuccess = "Validation successful"
-	var validatedFailed = "Validation failed, bad user/api key!"
+	let validatedSuccess = "Validation successful"
+	let validatedFailed = "Validation failed, bad user/api key!"
 	if (validation == "1") {
-		Homey.manager('settings').set('pushovervalidation', validatedSuccess);
+		Homey.ManagerSettings.set('pushovervalidation', validatedSuccess);
 	} else if (validation == "0") {
-		Homey.manager('settings').set('pushovervalidation', validatedFailed);
+		Homey.ManagerSettings.set('pushovervalidation', validatedFailed);
 	}
 }
 
-Homey.manager('flow').on('action.pushoverSend', function( callback, args ){
-	  if( typeof validation == 'undefined' || validation == '0') return callback( new Error("Pushover api/token key not configured or valid under settings!") );
-		var tempUser = pushoverUser;
-		var tempToken = pushoverToken;
-		var pMessage = args.message;
-		var sound = args.sound;
-		if( typeof pMessage == 'undefined' ||pMessage == null || pMessage == '') return callback( new Error("Message can not be empty") );
-		var pPriority = args.priority;
-		pushoverSend ( tempUser, tempToken, pMessage, pPriority, sound);
-    callback( null, true ); // we've fired successfully
-});
-
-Homey.manager('flow').on('action.pushoverSend_device', function( callback, args ){
-	  if( typeof validation == 'undefined' || validation == '0') return callback( new Error("Pushover api/token key not configured or valid under settings!") );
-		var tempUser = pushoverUser;
-		var tempToken = pushoverToken;
-		var pMessage = args.message;
-		var sound = args.sound
-		if( typeof pMessage == 'undefined' ||pMessage == null || pMessage == '') return callback( new Error("Message can not be empty") );
-		var pDevice = args.device.name;
-		if( pDevice == null || pDevice == '') return callback( new Error("No devices registered on this Pushover account!") );
-		var pPriority = args.priority;
-		pushoverSend_device ( tempUser, tempToken, pMessage, pDevice, pPriority, sound );
-    callback( null, true ); // we've fired successfully
-});
-
-Homey.manager('flow').on('action.pushoverSend_device.device.autocomplete', function( callback, value ) {
-	var deviceSearchString = value.query;
-	var items = searchForDevicesByValue( deviceSearchString );
-	callback( null, items );
-});
-
-// Send notification with parameters
-function pushoverSend ( pUser, pToken , pMessage, pPriority, sound) {
-	var priority = 0;
-	switch (pPriority) {
-		case 'Normal':
-			priority = 0;
-			break;
-		case 'Lowest':
-			priority = -2;
-			break;
-		case 'Low':
-			priority = -1;
-			break;
-		case 'High':
-			priority = 1;
-			break;
-	}
-	if (pToken != ""){
-
-	var p = new push( {
-		user: pUser,
-		token: pToken,
-	});
-
-	var msg = {
-		// These values correspond to the parameters detailed on https://pushover.net/api
-		// 'message' is required. All other values are optional.
-		message: pMessage,   // required
-		title: "Homey",
-		priority: priority,
-		sound: sound
-	};
-
-	p.send( msg, function( err, result ) {
-		if ( err ) {
-			throw err;
-		} else {
-			if (ledringPreference == true){
-				LedAnimate("green", 3000);
-			}
-		}
-		Homey.log( result );
-		//Add send notification to Insights
-		Homey.manager('insights').createEntry( 'pushover_sendNotifications', 1, new Date(), function(err, success){
-        if( err ) return Homey.error(err);
-    });
-	});
-	} else {
-		if (ledringPreference == true){
-			LedAnimate("red", 3000);
-		}
-	}
-}
-
-// Send notification with parameters
-function pushoverSend_device ( pUser, pToken , pMessage, pDevice, pPriority, sound) {
-	var priority = 0;
-	switch (pPriority) {
-		case 'Normal':
-			priority = 0;
-			break;
-		case 'Lowest':
-			priority = -2;
-			break;
-		case 'Low':
-			priority = -1;
-			break;
-		case 'High':
-			priority = 1;
-			break;
-	}
-	if (pToken != ""){
-	var p = new push( {
-		user: pUser,
-		token: pToken,
-	});
-
-	var msg = {
-		// These values correspond to the parameters detailed on https://pushover.net/api
-		// 'message' is required. All other values are optional.
-		message: pMessage,   // required
-		title: "Homey",
-		device: pDevice,
-		priority: priority,
-		sound: sound
-	};
-
-	p.send( msg, function( err, result ) {
-		if ( err ) {
-			throw err;
-		} else {
-			if (ledringPreference == true){
-				LedAnimate("green", 3000);
-			}
-		}
-		Homey.log( result );
-	});
-	} else {
-		if (ledringPreference == true){
-			LedAnimate("red", 3000);
-		}
-	}
-}
-
-function searchForDevicesByValue ( value ) {
-	var possibleDevices = devices;
-	var tempItems = [];
-	for (var i = 0; i < devices.length; i++) {
-		var tempDevice = possibleDevices[i];
-		if ( tempDevice.indexOf(value) >= 0 ) {
-			tempItems.push({ icon: "", name: tempDevice });
-		}
-	}
-	return tempItems;
-}
-
-
-function LedAnimate(colorInput, duration) {
-Homey.manager('ledring').animate(
-    // animation name (choose from loading, pulse, progress, solid)
-    'pulse',
-
-    // optional animation-specific options
-    {
-
-	   color: colorInput,
-        rpm: 300 // change rotations per minute
-    },
-
-    // priority
-    'INFORMATIVE',
-
-    // duration
-    duration,
-
-    // callback
-    function( err, success ) {
-        if( err ) return Homey.error(err);
-
-    }
-);
-}
-
-// Create Insight log
-function createInsightlog() {
-	Homey.manager('insights').createLog( 'pushover_sendNotifications', {
-    label: {
-        en: 'Send Notifications'
-    },
-    type: 'number',
-    units: {
-        en: 'notifications'
-    },
-    decimals: 0
-});
-}
-
-var self = module.exports = {
-	init: function () {
-
-		// Start building Pushover accounts array
-		buildPushoverArray();
-
-		createInsightlog();
-
-		Homey.manager('settings').on( 'set', function(settingname){
-
-			if(settingname == 'pushoveraccount') {
-			Homey.log('Pushover - Account has been changed/updated...');
-			buildPushoverArray();
-		}
-		});
-
-	}
-}
+module.exports = MyApp;
